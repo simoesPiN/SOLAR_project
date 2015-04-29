0187e2561fc5be733cefe94f8e922d477742a7fb
ketlinfabri <ketlin.fabri@gmail.com>
2015-04-29 17:21:25 -0400

    updated
    update

---
+++ Solar_Java/src/Inclination.java
@@ -0,0 +1,29 @@
+
+public class Inclination {
+	
+	private double latitude;
+	private String season;
+	private int day;
+	
+	double getLatitude() {
+		return latitude;
+	}
+	void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	String getSeason() {
+		return season;
+	}
+	void setSeason(String season) {
+		this.season = season;
+	}
+	
+	int getDay() {
+		return day;
+	}
+	void setDay(int day) {
+		this.day = day;
+	}
+
+}
